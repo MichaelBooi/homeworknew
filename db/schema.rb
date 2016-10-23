b18cbf2836a21f347cd1f4f72894594a037696e6
MichaelBooi <michaelbooi@hotmail.com>
2016-10-23 17:28:33 +0200

    show and controller songs

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 20161017142113) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "artists", force: :cascade do |t|
     t.string   "name"
     t.datetime "created_at", null: false
@@ -23,7 +26,7 @@ ActiveRecord::Schema.define(version: 20161017142113) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer  "artist_id"
-    t.index ["artist_id"], name: "index_songs_on_artist_id"
+    t.index ["artist_id"], name: "index_songs_on_artist_id", using: :btree
   end
 
 end
